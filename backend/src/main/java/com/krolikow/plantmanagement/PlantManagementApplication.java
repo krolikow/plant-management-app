4fc8c109141e6e9670bc268465509fef95caa82c
Magdalena <mkrolikowska01@poczta.fm>
2024-02-17 16:12:49 +0100

    ref: name change

--- backend/src/main/java/com/krolikow/plantmanagement/PlantManagementApplication.java
+++ backend/src/main/java/com/krolikow/plantmanagement/PlantManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlantManagementAppApplication {
+public class PlantManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlantManagementAppApplication.class, args);
+		SpringApplication.run(PlantManagementApplication.class, args);
 	}
 
 }
