fdb9fe92002b77a3db9eac3404f7276cb8587870
Magdalena <mkrolikowska01@poczta.fm>
2024-02-17 19:49:42 +0100

    feat: backend and frontend communication

--- backend/src/main/java/com/krolikow/plantmanagement/dao/PlantRepository.java
+++ backend/src/main/java/com/krolikow/plantmanagement/dao/PlantRepository.java
@@ -2,6 +2,8 @@ package com.krolikow.plantmanagement.dao;
 
 import com.krolikow.plantmanagement.entity.Plant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface PlantRepository extends JpaRepository<Plant, Long> {
 }
--- frontend/src/app/plant/plant-list/plant-list.component.ts
+++ frontend/src/app/plant/plant-list/plant-list.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PlantService} from "../plant.service";
+import {Plant} from "../plant.model";
 
 @Component({
   selector: 'app-plant-list',
-  standalone: true,
-  imports: [],
   templateUrl: './plant-list.component.html',
   styleUrl: './plant-list.component.css'
 })
-export class PlantListComponent {
+export class PlantListComponent implements OnInit {
+  plants: Plant[] = []
 
+  constructor(private plantService: PlantService) {
+  }
+
+  ngOnInit(): void {
+    this.listPlants();
+  }
+
+  listPlants() {
+    this.plantService.getPlantList().subscribe(
+      data => {
+        this.plants = data;
+      }
+    )
+  }
 }
