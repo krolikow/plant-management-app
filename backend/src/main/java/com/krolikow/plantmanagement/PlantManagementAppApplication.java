f17d6938cd24ef962965ac3c1024e4bfc6610912
Magdalena <mkrolikowska01@poczta.fm>
2024-02-17 16:01:45 +0100

    feat: backend skeleton

---
+++ backend/src/main/java/com/krolikow/plantmanagement/PlantManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.krolikow.plantmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantManagementAppApplication.class, args);
+	}
+
+}
