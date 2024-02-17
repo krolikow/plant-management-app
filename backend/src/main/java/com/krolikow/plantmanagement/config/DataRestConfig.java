9b4ecbef83f6a19e39403f7f6b335e8b2010a94a
Magdalena <mkrolikowska01@poczta.fm>
2024-02-17 16:06:41 +0100

    feat: read-only api

---
+++ backend/src/main/java/com/krolikow/plantmanagement/config/DataRestConfig.java
@@ -0,0 +1,29 @@
+package com.krolikow.plantmanagement.config;
+
+import com.krolikow.plantmanagement.entity.Plant;
+import com.krolikow.plantmanagement.entity.PlantCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        //read-only access
+        config.getExposureConfiguration()
+                .forDomainType(Plant.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+        //read-only access
+        config.getExposureConfiguration()
+                .forDomainType(PlantCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+
+    }
+}
